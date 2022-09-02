e5dad2c74361d62f087e70375bf9f9c891b88d37
MD Pahlovi <md.pahlovi07@gmail.com>
2022-09-03 03:24:15 +0600

    Card Added

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -24,5 +24,32 @@ function loadNewsData(item) {
 function loadNewsCard(newsData) {
     const totalNews = document.getElementById("total-news");
     totalNews.innerText = `${newsData.length}`;
-    console.log(newsData);
+    document.getElementById("news-card").innerHTML = "";
+    newsData.map((newsItem) => {
+        const newsCard = document.getElementById("news-card");
+        const div = document.createElement("div");
+        div.classList.add("flex", "flex-col", "items-center", "sm:flex-row", "border", "rounded-xl");
+        div.innerHTML = `
+            <img class="w-full sm:w-[244px] sm:h-[300px]" src=${newsItem.thumbnail_url} alt="" />
+            <div class="p-6 flex flex-col justify-start gap-3">
+                <h5 class="text-xl md:text-2xl font-medium">${newsItem.title}</h5>
+                <p class="max-h-24 overflow-hidden text-ellipsis">
+                ${newsItem.details.slice(0, 400)}</p>
+                <div class="flex flex-wrap gap-x-4 gap-y-3 justify-between items-center">
+                    <div class="flex gap-2">
+                        <img class="w-10 rounded-full" src=${newsItem.author.img} alt="" />
+                        <div>
+                            <h5 class="font-medium">${newsItem.author.name}</h5>
+                            <p class="text-sm">
+                            ${newsItem.author.published_date?.split(" ")[0]}</p>
+                        </div>
+                    </div>
+                    <h5 class="font-medium">${newsItem.total_view}</h5>
+                    <button class="group btn btn-secondary">Read More <span class="btn-animation"></span>
+                    </button>
+                </div>
+            </div>`;
+        newsCard.appendChild(div);
+        console.log(newsItem);
+    });
 }
