6223660c0e22ef0ca0bd6ef4904354b17731128d
MD Pahlovi <md.pahlovi07@gmail.com>
2022-09-02 23:28:47 +0600

    News Category Navigation Added

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,1 +1,13 @@
-console.log("Scripts Work");
+fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then((response) => response.json())
+    .then((data) => newsNav(data.data.news_category));
+
+function newsNav(navItems) {
+    navItems.map((item) => {
+        const newsCategory = document.getElementById("news-category");
+        const h3 = document.createElement("h3");
+        h3.classList.add("nav-news");
+        h3.innerText = `${item.category_name}`;
+        newsCategory.appendChild(h3);
+    });
+}
