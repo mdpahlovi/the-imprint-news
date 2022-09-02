e4c80603e5d11811fed21d05254b7b237607fd47
MD Pahlovi <md.pahlovi07@gmail.com>
2022-09-03 01:01:49 +0600

    All Category News Loaded in Console

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,13 +1,28 @@
 fetch("https://openapi.programming-hero.com/api/news/categories")
     .then((response) => response.json())
-    .then((data) => newsNav(data.data.news_category));
+    .then((newsCategoryData) => newsNav(newsCategoryData.data.news_category));
 
 function newsNav(navItems) {
     navItems.map((item) => {
         const newsCategory = document.getElementById("news-category");
-        const h3 = document.createElement("h3");
-        h3.classList.add("nav-news");
-        h3.innerText = `${item.category_name}`;
-        newsCategory.appendChild(h3);
+        const div = document.createElement("div");
+        div.innerHTML = `<h3 class="nav-news" onclick="loadNewsData('${item.category_id},${item.category_name}')">${item.category_name}</h3>`;
+        newsCategory.appendChild(div);
     });
+}
+
+function loadNewsData(item) {
+    const itemArray = item.split(",");
+    const newsCategory = document.getElementById("category");
+    newsCategory.innerText = `${itemArray[1]}`;
+    const url = `https://openapi.programming-hero.com/api/news/category/${itemArray[0]}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((newsData) => loadNewsCard(newsData.data));
+}
+
+function loadNewsCard(newsData) {
+    const totalNews = document.getElementById("total-news");
+    totalNews.innerText = `${newsData.length}`;
+    console.log(newsData);
 }
